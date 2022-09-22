5d95409e31260885bf06919b0babe04003f72176
Md. Fazlay Rabby <rabby.neo@gmail.com>
2022-09-23 05:35:33 +0600

    small design and pattern change.

--- OrderSystem/Startup.cs
+++ OrderSystem/Startup.cs
@@ -55,7 +55,7 @@ namespace OrderSystem
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Order}/{action=Index}/{id?}");
             });
         }
     }
